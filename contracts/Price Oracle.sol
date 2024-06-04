328afd9de10eac9ecc535c49536d238890c435a6
MuhammadHammadAhmed <42751169+MuhammadHammadAhmed@users.noreply.github.com>
2024-06-04 19:17:49 +0500

    commenced working on price oracle

---
+++ contracts/Price Oracle.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <=0.8.0;
+
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract PriceOracle {
+  
+    // address public pool;
+    // uint32 public observationPeriod; // e.g., 3600 for 1 hour
+
+    constructor(address _factory) {
+     
+
+    }
+
+    function getPrice(address _pool, uint32 _observationPeriod) external view returns (uint256) {
+      IUniswapV3Pool  pool=IUniswapV3Pool(_pool);
+       IERC20 token0=IERC20(pool.token0());
+       IERC20  token1=IERC20(pool.token1());
+
+        (int24 tick, ) = OracleLibrary.consult(_pool, _observationPeriod);
+        uint256 price = OracleLibrary.getQuoteAtTick(tick, 1 ether, pool.token1(), IUniswapV3Pool(_pool).token0());
+        return price;
+    }
+}
---
+++ test/priceOracle.js
@@ -0,0 +1,23 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const YOUR_UNISWAP_FACTORY_ADDRESS="0x1F98431c8aD98523631AE4a59f267346ea31F984";
+const USDC_WETH='0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8';
+const USDC_USDT='0x3416cf6c708da44db2624d63ea0aaef7113527c6';
+
+describe("PriceORACLE", function(){
+    it("Should deploy a TOracle contract", async function () {
+        const observationPeriod = 1; // 1 hour
+        /* Deploy the Oracle contract */
+        const OracleFActory = await hre.ethers.getContractFactory("PriceOracle");
+                const oracleContract = await OracleFActory.deploy(YOUR_UNISWAP_FACTORY_ADDRESS);
+                await oracleContract.deployed();
+        const response= await oracleContract.getPrice(USDC_WETH, observationPeriod);
+        console.log("PriceOracle response",response);
+        
+
+    });
+});
+
+
+
